dc15ca56199cb6b7a1cfc4e3f261610fdca2f0a0
phamhiep2506 <phamhiep25062k@gmail.com>
2024-11-02 19:58:26 +0700

    add progress bar

--- dl.py
+++ dl.py
@@ -4,6 +4,7 @@ import requests
 import os
 import subprocess
 import argparse
+from tqdm import tqdm
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input", help = "Input url m3u8")
@@ -19,11 +20,17 @@ os.system(
 merge = open(f"{os.getcwd()}/merge.txt", "w")
 with open("url.txt", "r") as file:
     for i, line in enumerate(file):
-        print(f"Download {i}.png")
+        res = requests.get(line.strip(), stream=True)
+        total_size = int(res.headers.get("content-length", 0))
+        block_size = 1024
 
-        req = requests.get(line.strip())
-
-        open(f"{os.getcwd()}/ts/{i}.png", "wb").write(req.content)
+        with tqdm(total=total_size, unit="B", unit_scale=True, desc=f"{i}.png") as progress_bar:
+            with open(f"{os.getcwd()}/ts/{i}.png", "wb") as file:
+                for data in res.iter_content(block_size):
+                    progress_bar.update(len(data))
+                    file.write(data)
+        if total_size != 0 and progress_bar.n != total_size:
+            raise RuntimeError("Could not download file")
 
         xxd_png = "89504e470d0a1a0a0000000d494844520000000100000001010300000025db56ca00000003504c5445000000a77a3dda0000000174524e530040e6d8660000000a4944415408d76360000000020001e221bc330000000049454e44ae4260"
         xxd_ts = subprocess.run(
