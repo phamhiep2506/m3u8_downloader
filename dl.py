5f068e484c8306c1dbb0b8d7492bd3219721a7dc
phamhiep2506 <phamhiep25062k@gmail.com>
2024-11-03 13:08:28 +0700

    add referer request

--- dl.py
+++ dl.py
@@ -7,8 +7,8 @@ import re
 import shutil
 
 
-def download_ts(url: str, index: int):
-    res = requests.get(url)
+def download_ts(url: str, referer: str, index: int):
+    res = requests.get(url, headers={"Referer": referer})
 
     ts = open(f"{os.getcwd()}/ts/{index}.ts", "wb")
     png = b"\x89\x50\x4e\x47\x0d\x0a\x1a\x0a\x00\x00\x00\x0d\x49\x48\x44\x52\x00\x00\x00\x01\x00\x00\x00\x01\x01\x03\x00\x00\x00\x25\xdb\x56\xca\x00\x00\x00\x03\x50\x4c\x54\x45\x00\x00\x00\xa7\x7a\x3d\xda\x00\x00\x00\x01\x74\x52\x4e\x53\x00\x40\xe6\xd8\x66\x00\x00\x00\x0a\x49\x44\x41\x54\x08\xd7\x63\x60\x00\x00\x00\x02\x00\x01\xe2\x21\xbc\x33\x00\x00\x00\x00\x49\x45\x4e\x44\xae\x42\x60"
@@ -31,7 +31,7 @@ def download_m3u8(args):
             count = count + 1
             print(f"Downloading {count}/{total_line}")
             merge.write(f"file 'ts/{count}.ts'")
-            download_ts(line.decode("utf-8"), count)
+            download_ts(line.decode("utf-8"), args.referer, count)
 
     merge.close()
 
