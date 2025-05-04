54fea11fb5f78a2574f7d75262717984a1fd6db9
phamhiep2506 <phamhiep25062k@gmail.com>
2025-05-04 17:45:29 +0700

    update v2.0.0

--- dl.py
+++ dl.py
@@ -1,60 +1,86 @@
 #!/usr/bin/env python
 
+import argparse
+import glob
+import m3u8
+from tqdm import tqdm
 import requests
 import os
-import argparse
 import re
-import shutil
+import subprocess
 
 
-def download_ts(url: str, referer: str, index: int):
-    if os.path.exists(f"{os.getcwd()}/ts/{index}.ts") is True:
+REGEX_PNG = r"^[A-Za-z0-9].*44ae4260"
+
+
+def get_urls_inside_m3u8(url: str, referer: str) -> list[str]:
+    response = requests.get(url, headers={"Referer": referer}, stream=True)
+    playlist = m3u8.loads(response.text)
+    return playlist.files
+
+
+def png_to_ts(path_to_file_ts: str):
+    hex_string = subprocess.run(
+        ["xxd", "-ps", "-c", "0", path_to_file_ts], capture_output=True
+    )
+    hex_string_remove_png = re.sub(REGEX_PNG, "", hex_string.stdout.decode("utf-8"))
+    data = subprocess.run(
+        ["xxd", "-ps", "-r"], input=hex_string_remove_png.encode(), capture_output=True
+    )
+
+    os.remove(path_to_file_ts)
+
+    with open(path_to_file_ts, "wb") as file:
+        file.write(data.stdout)
+    file.close()
+
+
+def download_ts(url: str, referer: str, name_ts: str, output_path: str):
+    if os.path.exists(f"{output_path}/{name_ts}.ts") is True:
         return
 
-    res = requests.get(url, headers={"Referer": referer})
+    with requests.get(url, headers={"Referer": referer}, stream=True) as response:
+        response.raise_for_status()
+        with open(f"{output_path}/{name_ts}.ts", "wb") as file:
+            for chuck in response.iter_content(chunk_size=1024):
+                if chuck:
+                    file.write(chuck)
+    file.close()
 
-    ts = open(f"{os.getcwd()}/ts/{index}.ts", "wb")
-    # png_1x1_1bit
-    png_1x1 = b"\x89\x50\x4e\x47\x0d\x0a\x1a\x0a\x00\x00\x00\x0d\x49\x48\x44\x52\x00\x00\x00\x01\x00\x00\x00\x01\x01\x03\x00\x00\x00\x25\xdb\x56\xca\x00\x00\x00\x03\x50\x4c\x54\x45\x00\x00\x00\xa7\x7a\x3d\xda\x00\x00\x00\x01\x74\x52\x4e\x53\x00\x40\xe6\xd8\x66\x00\x00\x00\x0a\x49\x44\x41\x54\x08\xd7\x63\x60\x00\x00\x00\x02\x00\x01\xe2\x21\xbc\x33\x00\x00\x00\x00\x49\x45\x4e\x44\xae\x42\x60"
-    # png_1x1_8bit = b"\x89\x50\x4e\x47\x0d\x0a\x1a\x0a\x00\x00\x00\x0d\x49\x48\x44\x52\x00\x00\x00\x01\x00\x00\x00\x01\x08\x02\x00\x00\x00\x90\x77\x53\xde\x00\x00\x00\x10\x49\x44\x41\x54\x78\x9c\x62\xfa\xff\xff\x3f\x20\x00\x00\xff\xff\x06\x06\x03\x00\xb7\x66\x11\x21\x00\x00\x00\x00\x49\x45\x4e\x44\xae\x42\x60"
-    ts.write(res.content.replace(png_1x1, b""))
-    ts.close()
-
-
-def download_m3u8(url: str, referer: str):
-    count = 0
-    total_line = 0
-    res = requests.get(url, headers={"Referer": referer})
-    for line in res.iter_lines():
-        if re.search("https", line.decode("utf-8")):
-            total_line = total_line + 1
-
-    merge = open(f"{os.getcwd()}/merge.txt", "w")
-
-    for line in res.iter_lines():
-        if re.search("https", line.decode("utf-8")):
-            count = count + 1
-            print(f"Downloading {count}/{total_line}")
-            merge.write(f"file 'ts/{count}.ts'")
-            merge.write("\n")
-            download_ts(line.decode("utf-8"), referer, count)
-
-    merge.close()
+    png_to_ts(f"{output_path}/{name_ts}.ts")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input", help="Input url m3u8")
-    parser.add_argument("-r", "--referer", help="Referer url")
-    parser.add_argument("-c", "--continued", action="store_true", help="Continue downloading")
+    parser.add_argument("-i", "--input", help="Input url m3u8", required=True)
+    parser.add_argument("-r", "--referer", help="Referer url", required=False)
+    parser.add_argument(
+        "-c",
+        "--continued",
+        action="store_true",
+        help="Continue downloading",
+        required=False,
+    )
+    parser.add_argument("-o", "--output", help="/path/to/file.mp4", required=True)
     args = parser.parse_args()
 
-    path_ts = os.path.join(os.getcwd(), "ts")
+    urls = get_urls_inside_m3u8(args.input, args.referer)
+
+    dir_output_ts = "ts"
+    file_merge = "merge.txt"
 
     if args.continued is False:
-        if os.path.exists(path_ts):
-            shutil.rmtree(path_ts)
-        if not os.path.exists(path_ts):
-            os.mkdir(path_ts)
+        if not os.path.exists(dir_output_ts):
+            os.mkdir(dir_output_ts)
 
-    download_m3u8(args.input, args.referer)
+        ts_files = glob.glob(f"{dir_output_ts}/*")
+        for file in ts_files:
+            os.remove(file)
+
+    with open(file_merge, "w") as file:
+        for index, url in enumerate(tqdm(urls)):
+            download_ts(url, args.referer, str(index), dir_output_ts)
+            file.write(f"file {dir_output_ts}/{index}.ts\n")
+    file.close()
+
+    os.system(f"ffmpeg -f concat -i merge.txt -c copy {args.output}")
