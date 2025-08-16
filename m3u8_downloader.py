2efe546a0874c19d2a284646f81037878110662e
phamhiep2506 <phamhiep25062k@gmail.com>
2025-08-16 22:19:07 +0700

    add ffmpeg convert

--- m3u8_downloader.py
+++ m3u8_downloader.py
@@ -4,25 +4,34 @@ import cloudscraper
 import m3u8
 import re
 import os
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from requests.utils import parse_header_links
+from tqdm import tqdm
+import argparse
+import shutil
+import ffmpeg
 
-URL_M3U8 = "https://stream.cliphot69.one/video2/85e9c82a-05df-4946-a00e-97736b38b94a/master.html"
+URL_M3U8 = ""
+LIST_DOWNLOAD_FILES = []
+MAX_WORKERS = 0
+DIR_DOWNLOAD_TS = ""
+MERGE_FILE = ""
 
 
 def data_to_file(data, file_name):
 
-    if not os.path.exists("ts"):
+    if not os.path.exists(DIR_DOWNLOAD_TS):
         try:
-            os.mkdir("ts")
+            os.mkdir(DIR_DOWNLOAD_TS)
         except Exception as error:
-            print(error)
+            print(f"Create directory ts: {error}")
 
     try:
-        with open(f"ts/{file_name}", "wb") as f:
+        with open(f"{DIR_DOWNLOAD_TS}/{file_name}", "wb") as f:
             f.write(data)
             f.close()
     except Exception as error:
-        print(error)
+        print(f"Data to file: {error}")
 
 
 def png_to_ts(data, file_name):
@@ -32,34 +41,114 @@ def png_to_ts(data, file_name):
 
 
 def merge_ts(file_name):
-    with open("merge.txt", "a") as f:
+    with open(MERGE_FILE, "a") as f:
         f.write(f"file ts/{file_name}\n")
         f.close()
 
 
 def download_ts(url, file_name):
+    if os.path.exists(f"{DIR_DOWNLOAD_TS}/{file_name}"):
+        return
+
     try:
         scraper = cloudscraper.create_scraper()
         data = scraper.get(url)
         png_to_ts(data.content, file_name)
-        merge_ts(file_name)
     except Exception as error:
-        print(error)
+        print(f"Download ts: {error}")
 
 
-files = []
-try:
-    scraper = cloudscraper.create_scraper()
-    response = scraper.get(URL_M3U8)
+def download_multiple_files(urls_ts):
+    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+        futures = [
+            executor.submit(download_ts, url, file_name) for url, file_name in urls_ts
+        ]
 
-    playlist = m3u8.loads(f"{response.text}")
+        with tqdm(
+            total=len(futures),
+            desc="Downloading files",
+            bar_format="{l_bar}{bar}| {n_fmt}/{total_fmt}",
+        ) as progress_bar:
+            for future in as_completed(futures):
+                try:
+                    progress_bar.update(1)
+                except Exception as error:
+                    print(f"Download multiple files: {error}")
+                    print(future)
 
-    for index, url in enumerate(playlist.segments.uri):
-        if os.path.exists(f"ts/{index}.ts"):
-            continue
-        files.append((url, f"{index}.ts"))
-except Exception as error:
-    print(error)
 
-with ThreadPoolExecutor(max_workers=10) as executor:
-    [executor.submit(download_ts, url, file_name) for url, file_name in files]
+def ffmpeg_concat_ts(merge_file, output):
+    ffmpeg.input(merge_file, format="concat", safe=0).output(output, c="copy").run()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="The program downloads ts from m3u8 link and converts png ts to ts file"
+    )
+    parser.add_argument(
+        "-i", "--input", type=str, help="Link download file m3u8", required=True
+    )
+    parser.add_argument(
+        "-n",
+        "--new",
+        action="store_true",
+        help="Download new data",
+    )
+    parser.add_argument(
+        "-w",
+        "--worker",
+        type=int,
+        help="Maximum number of workers when downloading files",
+        default=5,
+    )
+    parser.add_argument(
+        "-o",
+        "--output_ts",
+        type=str,
+        help="Output directory when downloading ts file from m3u8",
+        default="ts",
+    )
+    parser.add_argument(
+        "-O",
+        "--output_mp4",
+        type=str,
+        help="Path and file name when converting to mp4",
+        required=True,
+    )
+    args = parser.parse_args()
+
+    URL_M3U8 = args.input
+    MAX_WORKERS = args.worker
+    DIR_DOWNLOAD_TS = args.output_ts
+    file_name_mp4 = os.path.basename(args.output_mp4).split("/")[-1]
+    MERGE_FILE = f"{os.path.splitext(file_name_mp4)[0]}_merge.txt"
+
+    if os.path.exists(MERGE_FILE):
+        try:
+            os.remove(MERGE_FILE)
+        except Exception as error:
+            print(f"Remove file {MERGE_FILE}: {error}")
+
+    if args.new:
+        if os.path.exists(DIR_DOWNLOAD_TS):
+            try:
+                shutil.rmtree(DIR_DOWNLOAD_TS)
+            except Exception as error:
+                print(f"Remove directory ts: {error}")
+
+    try:
+        scraper = cloudscraper.create_scraper()
+        response = scraper.get(URL_M3U8)
+
+        playlist = m3u8.loads(f"{response.text}")
+
+        for index, url in enumerate(playlist.segments.uri):
+            LIST_DOWNLOAD_FILES.append((url, f"{index}.ts"))
+            merge_ts(f"{index}.ts")
+    except Exception as error:
+        print(f"Get M3U8: {error}")
+
+    download_multiple_files(LIST_DOWNLOAD_FILES)
+
+    if args.output_mp4 != "":
+        ffmpeg_concat_ts(MERGE_FILE, args.output_mp4)
